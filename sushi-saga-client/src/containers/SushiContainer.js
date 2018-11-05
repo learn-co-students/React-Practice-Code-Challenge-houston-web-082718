d602a31944aa656a372fe25ebad38ebe964e78b1
V10LET <violet.wmoon@gmail.com>
2018-11-04 19:43:02 -0600

    made the sushi app

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,17 @@
-import React, { Fragment } from 'react'
+import React, {Fragment, Component} from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+export default class SushiContainer extends Component {
+
+    render() {
+        const {sushis, plateCount, handleMore, walletValue} = this.props
+        return (<Fragment>
+            <div className="belt">
+                {sushis.map(sushi => <Sushi key={sushi.id} sushi={sushi} plateCount={plateCount} walletValue={walletValue}/>)}
+                <MoreButton handleMore={handleMore}/>
+            </div>
+
+        </Fragment>)
+    }
 }
-
-export default SushiContainer
